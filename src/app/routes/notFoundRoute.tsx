174de456d36e16799b6f4be452ad0638bfa4c9be
Elebesunu Osereme <elebesunuosereme@gmail.com>
2025-09-23 10:27:54 +0100

    fix: text align issue in 404

--- src/app/routes/notFoundRoute.tsx
+++ src/app/routes/notFoundRoute.tsx
@@ -5,9 +5,9 @@ const NotFoundRoute = () => {
   return (
     <div className="min-h-screen lg:pt-(--spacing-600) 2xl:px-(--spacing-1400) lg:px-(--spacing-600) px-(--spacing-200) pt-(--spacing-200) lg:pb-(--spacing-1000) pb-(--spacing-600)'>">
       <Navbar />
-      <main className='flex flex-col gap-(--spacing-300) items-center pt-(--spacing-500)'>
+      <main className='flex flex-col gap-(--spacing-300) text-center items-center pt-(--spacing-500)'>
         <h1>
-          <h1 className='text-present-2 font-dm-sans-light'>
+          <h1 className='lg:text-present-2 text-present-3 font-dm-sans-light'>
             <span className='font-dm-sans-bold'>404</span> - Not Found
           </h1>
         </h1>
